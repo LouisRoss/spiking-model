077e2da1f7fef66e1d8e1ae4b98b0fe6e1542cf5
Louis Ross <louis.ross@gmail.com>
2021-01-30 12:57:47 -0800

    Generate streaming input in python

--- spiking-model/ui/nn.py
+++ spiking-model/ui/nn.py
@@ -9,6 +9,8 @@ import nnclean
 import nnplot
 import nntidy
 
+python_interpreter = 'python'
+
 def load_configuration():
     conf = configuration()
     if 'PostProcessing' not in conf.configuration:
@@ -23,16 +25,39 @@ def load_configuration():
     Path(record_path).mkdir(parents=True, exist_ok=True)
     return conf
 
+def generate_input_stream(conf):
+    global python_interpreter
+    argv = []
+
+    if 'Model' not in conf.configuration:
+        print('Required "Model" section not in configuration file')
+        return
+
+    if 'InputStreamFileGenerator' not in conf.configuration['Model']:
+        print('Required subkey "InputStreamFileGenerator" not in configuration file "Model" section')
+        return
+
+    if 'PythonInterpreter' in conf.settings:
+        python_interpreter = conf.settings['PythonInterpreter']
+
+    print('Using python interpreter ' + python_interpreter + ' for input stream generation')
+    argv.append(python_interpreter)
+
+    argv.append(conf.configuration['Model']['InputStreamFileGenerator'])
+    argv.append(sys.argv[1])
+
+    subprocess.run(argv)
+
 def parse_arguments(conf):
     argv = []
 
     if 'Execution' not in conf.configuration:
         print('Required "Execution" section not in configuration file')
-        return
+        return argv
 
     if 'ModelExecutable' not in conf.configuration['Execution']:
         print('Required subkey "ModelExecutable" not in configuration file "Execution" section')
-        return
+        return argv
 
     argv.append(conf.configuration['Execution']['ModelExecutable'])
 
@@ -49,6 +74,7 @@ def parse_arguments(conf):
 
 def run():
     conf = load_configuration()
+    generate_input_stream(conf)
     argv = parse_arguments(conf)
     subprocess.run(argv)
     
